7f0ca7c721f3e4e271d2d5c9b5319198108ace9f
Sra-Estela <aureanobregacalixtocal@gmail.com>
2024-12-26 16:45:29 -0300

    Atualização

--- lista3/app.py
+++ lista3/app.py
@@ -1,15 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, make_response, jsonify
+from bd import Dados
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Página Inicial"
+    return render_template('index.html')
 
-@app.route('/formulario', methods=['GET', 'POST'])
-def feedback():
+@app.route('/formulario')
+def formulario():
+    return render_template('formulario.html')
+
+@app.route('/resposta', methods=['GET', 'POST'])
+def isolar_dados():
     if request.method == 'POST':
         nome = request.form.get('nome')
+        email = request.form.get('email')
         comentario = request.form.get('comentario')
-        return f"Obrigado pelo feedback, {nome}! Comentário recebido: {comentario}"
-    return render_template('formulario.html') 
+        novo_id = len(Dados) + 1
+        novo_coment = {
+            'id': novo_id,
+            'nome': nome,
+            'email': email,
+            'comentario': comentario
+        }
+        Dados.append(novo_coment)
+        return render_template('resposta.html', dado=novo_coment)
+        # return f'''
+        #             Obrigado pelo seu feedback, {nome}!
+        #             E-mail Confirmado: {email};
+        #             Comentário: {comentario}
+        #         ''' 
+    return render_template('index.html')
